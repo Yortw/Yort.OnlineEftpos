69c999f69b9216b1f1e795d1e47c2a3725ef4e19
Troy Willmot <yort@yortondotnet.com>
2016-12-02 11:31:07 +1300

    Add support for KnownBanks.

---
+++ Yort.OnlineEftpos.Net40.Tests/KnownBanksTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yort.OnlineEftpos.Net40.Tests
+{
+	[TestClass]
+	public class KnownBanksTests
+	{
+		[TestMethod]
+		public void KnownBanks_LoadsDefaultList()
+		{
+			Assert.IsTrue(KnownBanks.All.Count() > 0);
+		}
+
+		[TestMethod]
+		public void KnownBanks_LoadsAsbBank()
+		{
+			Assert.IsNotNull(KnownBanks.GetBank("ASB"));
+		}
+
+		[TestMethod]
+		public void KnownBanks_IdLookupIsCaseInsensitive()
+		{
+			Assert.IsNotNull(KnownBanks.GetBank("ASB"));
+			Assert.IsNotNull(KnownBanks.GetBank("Asb"));
+			Assert.IsNotNull(KnownBanks.GetBank("aSB"));
+			Assert.IsNotNull(KnownBanks.GetBank("aSb"));
+		}
+
+		#region AddBank Tests
+
+		[TestMethod]
+		public void KnownBanks_AddBank_AddsValidBankDefinition()
+		{
+			var id = System.Guid.NewGuid().ToString();
+			var newBank = new BankDefinition(id, new List<string>(1) { "Mobile" });
+
+			var oldCount = KnownBanks.All.Count();
+			Assert.IsTrue(KnownBanks.AddBank(newBank));
+			Assert.AreEqual(oldCount + 1, KnownBanks.All.Count());
+			Assert.IsNotNull(KnownBanks.GetBank(id));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void KnownBanks_AddBank_ThrownOnNullBank()
+		{
+			KnownBanks.AddBank(null);
+		}
+
+		#endregion
+
+		#region AddBank Tests
+
+		[TestMethod]
+		public void KnownBanks_RemoveBank_RemovesBankById()
+		{
+			var id = System.Guid.NewGuid().ToString();
+			var newBank = new BankDefinition(id, new List<string>(1) { "Mobile" });
+			KnownBanks.AddBank(newBank);
+
+			var oldCount = KnownBanks.All.Count();
+			Assert.IsTrue(KnownBanks.RemoveBank(id));
+
+			Assert.AreEqual(oldCount - 1, KnownBanks.All.Count());
+			Assert.IsNull(KnownBanks.GetBank(id));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void KnownBanks_AddBank_ThrownOnNullBankId()
+		{
+			KnownBanks.RemoveBank(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void KnownBanks_AddBank_ThrownOnEmptyBankId()
+		{
+			KnownBanks.RemoveBank(String.Empty);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void KnownBanks_AddBank_ThrownOnWhitespaceBankId()
+		{
+			KnownBanks.RemoveBank("   ");
+		}
+
+		#endregion
+
+	}
+}
---
+++ Yort.OnlineEftpos.SharedContracts/KnownBanks.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yort.OnlineEftpos
+{
+	/// <summary>
+	/// Provides a list of banks known to be supported, and the payer id types each one supports. 
+	/// </summary>
+	/// <remarks>
+	/// <para>As this list is hard coded into the library it may or may not be up to date at any given point in time. It should be used only as a UI guide, and not
+	/// a validation list/authority.</para>
+	/// <para>The list is exposed as modifiable collections so if an authoritive source becomes available, this data can be modified at runtime.</para>
+	/// </remarks>
+	public static class KnownBanks
+	{
+
+		#region Fields
+
+		private static ConcurrentDictionary<string, BankDefinition> _Banks;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Static constructor which sets up known banks.
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification="Need to load the actual content anyway, so no perf gain by following rule.")]
+		static KnownBanks()
+		{
+			_Banks = new ConcurrentDictionary<string, BankDefinition>(StringComparer.OrdinalIgnoreCase);
+			
+			AddBank(new BankDefinition("ASB", new List<string>(1) { "MOBILE" }));
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Adds a <see cref="BankDefinition"/> to the set of known, supported banks.
+		/// </summary>
+		/// <param name="bank">A <see cref="BankDefinition"/> instance to be added to the collection of known banks.</param>
+		/// <returns>True if the item was succesfully added, otherwise false (usually because the id is a duplicate).</returns>
+		/// <exception cref="System.ArgumentException">Thrown if the id of the <paramref name="bank"/> reference is null, empty or only whitespace.</exception>
+		/// <exception cref="System.ArgumentNullException">Thrown if <paramref name="bank"/> is null.</exception>
+		public static bool AddBank(BankDefinition bank)
+		{
+			bank.GuardNull(nameof(bank));
+			bank.Id.GuardNullEmptyOrWhitespace("bank.Id");
+
+			return _Banks.TryAdd(bank.Id, bank);
+		}
+
+		/// <summary>
+		/// Removes a bank from the list of known banks.
+		/// </summary>
+		/// <param name="id">The id of the bank defintion to remove.</param>
+		/// <returns>True if the item was removed, false if it was not found.</returns>
+		/// <exception cref="System.ArgumentException">Thrown if the id of the <paramref name="id"/> reference is null, empty or only whitespace.</exception>
+		public static bool RemoveBank(string id)
+		{
+			id.GuardNullEmptyOrWhitespace(nameof(id));
+
+			BankDefinition removedBank = null;
+			return _Banks.TryRemove(id, out removedBank);
+		}
+
+		/// <summary>
+		/// Returns the bank with the specified <paramref name="id"/>, or null if no bank with that id was found.
+		/// </summary>
+		/// <param name="id">The unique id of the bank object to retrieve.</param>
+		/// <returns>A <see cref="BankDefinition"/> containing details of the known bank, or null if no relevant bank was found.</returns>
+		public static BankDefinition GetBank(string id)
+		{
+			id.GuardNullEmptyOrWhitespace(nameof(id));
+
+			BankDefinition retVal = null;
+			_Banks.TryGetValue(id, out retVal);
+			return retVal;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// Returns all known banks as an <see cref="IEnumerable{T}"/> of <see cref="BankDefinition"/> instances.
+		/// </summary>
+		public static IEnumerable<BankDefinition> All { get { return _Banks.Values; } }
+
+		#endregion
+
+	}
+
+	/// <summary>
+	/// Represents a bank known to be supported by Online EFTPOS, and the associated payer id types that is supports.
+	/// </summary>
+	public class BankDefinition
+	{
+
+		/// <summary>
+		/// Partial constructor.
+		/// </summary>
+		/// <param name="id">The unique id of this bank, case insensitive. Also used for <see cref="DisplayName"/>.</param>
+		/// <param name="payerIdTypes">A list of strings containing allowed payer id types for this bank.</param>
+		/// <exception cref="System.ArgumentException">Thrown if <see cref="Id"/> is an empty string or whitespace.</exception>
+		/// <exception cref="System.ArgumentNullException">Thrown if <see cref="Id"/> or <see cref="PayerIdTypes"/> is null.</exception>
+		public BankDefinition(string id, IList<string> payerIdTypes) : this(id, null, payerIdTypes)
+		{
+		}
+
+		/// <summary>
+		/// Full constructor.
+		/// </summary>
+		/// <param name="id">The unique id of this bank, case insensitive.</param>
+		/// <param name="displayName">The display name of this bank. If null then <paramref name="id"/> will be used.</param>
+		/// <param name="payerIdTypes">A list of strings containing allowed payer id types for this bank.</param>
+		/// <exception cref="System.ArgumentException">Thrown if <see cref="Id"/> is an empty string or whitespace.</exception>
+		/// <exception cref="System.ArgumentNullException">Thrown if <see cref="Id"/> or <see cref="PayerIdTypes"/> is null.</exception>
+		public BankDefinition(string id, string displayName, IList<string> payerIdTypes)
+		{
+			id.GuardNullEmptyOrWhitespace(nameof(id));
+			payerIdTypes.GuardNull(nameof(displayName));
+
+			this.Id = id;
+			this.DisplayName = displayName ?? id;
+			this.PayerIdTypes = payerIdTypes;
+		}
+
+		/// <summary>
+		/// Returns the unique, case insensitive, id of this bank.
+		/// </summary>
+		/// <remarks>
+		/// <para>This must be the actual id passed to the Online EFTPOS API via <see cref="BankDetails.BankId"/> when making transaction requests.</para>
+		/// </remarks>
+		public string Id { get; private set; }
+
+		/// <summary>
+		/// Returns the display name of this bank.
+		/// </summary>
+		public string DisplayName { get; private set; }
+
+		/// <summary>
+		/// A list of payer id types supported by this bank.
+		/// </summary>
+		public IList<string> PayerIdTypes { get; private set; }
+	}
+}
